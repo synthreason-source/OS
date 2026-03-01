d60aa68a983e4ff2cf6427b8a8c29303a90f2993
synthreason-source <georgewagenknecht1@gmail.com>
2026-03-01 22:52:30 +1100

    build with ' dotnet build -c Release'

--- win/FileSecurityMonitor.cs
+++ win/FileSecurityMonitor.cs
@@ -7,7 +7,7 @@ using System.Management;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-
+using System.Diagnostics.Eventing.Reader;
 namespace FileSecurityMonitor
 {
     /// <summary>
--- win/LoggingAndETW.cs
+++ win/LoggingAndETW.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-
+using System.Diagnostics.Eventing.Reader;
 namespace FileSecurityMonitor
 {
     /// <summary>
